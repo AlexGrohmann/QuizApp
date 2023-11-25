2232fa9e38e7629553985612eb9609d3e0466f1c
AlexGrohmann <alex.grohmann@googlemail.com>
2023-11-25 20:08:49 +0100

    update Quiz Database

--- app/screens/QuizPage.js
+++ app/screens/QuizPage.js
@@ -14,7 +14,7 @@ import Questions from "./Questions";
 import { Colors } from "./Welcome";
 
 const QuizPage = ({ navigation }) => {
-  // version 4
+  // version 5
   const allQuestions = data;
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
